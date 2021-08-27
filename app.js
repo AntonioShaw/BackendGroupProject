3c7be024e329ed965a1cd2c38df57926fd6ec73c
David Burrell <d.burrell698@gmail.com>
2021-08-27 19:12:07 -0400

    added a registration mustache page and authentication middleware

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
-const stripe = require('stripe')('sk_test_51JT7KJE0bFkJrUC3x9ZcQ1Z6tECdWzqBDbrYcL1yAoB1Ledq5ghg6U3aGPjoeuviulnXSEWQxdo2glycIOfxbxrF00uUdyEbCs');
+const PORT = 3000
 const express = require('express');
 const app = express();
 
 const mustacheExpress = require('mustache-express');
+
 const path = require('path');
 
+const registerRouter = require('./routes/register')
+
+// import bcryptjs package
+global.bcrypt = require('bcryptjs')
+
+// import authenticate function
+const authenticate = require('./middleware/authentication')
+
+// create path for partial
+const VIEWS_PATH = path.join(__dirname, './views');
 
 
-const PATH = 3000
-const VIEWS_PATH = path.join(__dirname, );
+app.use(express.static('/public'));
 
-
-const YOUR_DOMAIN = 'http://localhost:8000';
-
-app.use(express.static('public'));
-
+// set up express to use mustache-express as template page
 app.engine ('mustache', mustacheExpress(VIEWS_PATH + '/partials','.mustache'));
+// set location of pages to views directory
 app.set('views', VIEWS_PATH);
+// set page extention to mustache
 app.set('view engine', 'mustache');
 
 
+app.use('/register', registerRouter)
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // TODO: replace this with the `price` of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    payment_method_types: [
-      'card',
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-  });
 
-  res.redirect(303, session.url)
+app.listen(PORT, () => {
+  console.log('Server is running... you better go catch it')
 });
-
-app.listen(4242, () => console.log('Running on port 4242'));
