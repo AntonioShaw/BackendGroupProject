bc4cf6bc353ac5ae776cae7a5cc7f240a24324bd
David Burrell <d.burrell698@gmail.com>
2021-08-28 11:06:30 -0400

    added ability to register user and send user info to Users table in database.

--- app.js
+++ app.js
@@ -6,29 +6,38 @@ const mustacheExpress = require('mustache-express');
 
 const path = require('path');
 
+global.models = require('./models')
+
+// import register.js route (DB)
 const registerRouter = require('./routes/register')
 
-// import bcryptjs package
+// import bcryptjs package (DB)
 global.bcrypt = require('bcryptjs')
 
-// import authenticate function
+// import authenticate function (DB)
 const authenticate = require('./middleware/authentication')
 
-// create path for partial
+// create path for partial (DB)
 const VIEWS_PATH = path.join(__dirname, './views');
 
 
-app.use(express.static('/public'));
 
-// set up express to use mustache-express as template page
+
+// set up express to use mustache-express as template page (DB)
 app.engine ('mustache', mustacheExpress(VIEWS_PATH + '/partials','.mustache'));
-// set location of pages to views directory
+// set location of pages to views directory (DB)
 app.set('views', VIEWS_PATH);
-// set page extention to mustache
+// set page extention to mustache (DB)
 app.set('view engine', 'mustache');
 
+// tell server to use urlencoded for body parsing (DB)
+app.use(express.urlencoded())
 
 app.use('/register', registerRouter)
+
+
+// set path for static css and js files (DB)
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(PORT, () => {
