913feee2a7d9d4d897f8c31006daed4934d0178e
AntonioShaw <tonyshaw0613@gmail.com>
2021-09-01 16:23:33 -0400

    Added grid displaying 12 shoes. Defined grid response.

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-
-// middleware fuction to authenticate user access
-function authenticate (req, res, next) {
-    console.log('Authenticate Middleware')
-    if(req.session) {
-        if(req.session.user) {
-            console.log(req.session.user)
-            res.locals.authenticated = true
-            next()
-        } else {
-            res.redirect('/login')
-        }
-    } else {
-        res.redirect('/login')
-    }
-}
-
-
-=======
 
 // middleware fuction to authenticate user access
 function authenticate (req, res, next) {
@@ -36,5 +16,4 @@ function authenticate (req, res, next) {
 }
 
 
->>>>>>> master
 module.exports = authenticate
--- models/shoetable.js
+++ models/shoetable.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class ShoeTable extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  ShoeTable.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    size: DataTypes.STRING,
-    style: DataTypes.STRING,
-    price: DataTypes.STRING,
-    image: DataTypes.BLOB
-  }, {
-    sequelize,
-    modelName: 'ShoeTable',
-  });
-  return ShoeTable;
-=======
 'use strict';
 const {
   Model
@@ -55,5 +26,4 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'ShoeTable',
   });
   return ShoeTable;
->>>>>>> master
 };
