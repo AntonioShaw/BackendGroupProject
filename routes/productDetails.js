2286f71a8f0e20a471bf76f0777bdb5a13d438d9
David Burrell <d.burrell698@gmail.com>
2021-09-02 10:57:29 -0400

    added js functionality to product details page

--- routes/productDetails.js
+++ routes/productDetails.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express()
 
 
-router.get('/', (req, res) => {
-    res.render('product-details')
+router.get('/:shoeId', async (req, res) => {
+
+    const shoeId = req.params.shoeId
+    const shoe = await models.ShoeTable.findByPk(shoeId)
+
+    res.render('product-details', shoe.dataValues)
 })
 
 
